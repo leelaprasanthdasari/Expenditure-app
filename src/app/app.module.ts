5607572e961d9f837f9043ad8d0eccdd16edbb42
leelaprasanthdasari <prasanthprince1998@gmail.com>
2020-06-27 00:09:45 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
+import {CalendarModule} from 'primeng/calendar';
+import {FullCalendarModule} from 'primeng/fullcalendar';
+import {DragDropModule} from 'primeng/dragdrop';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {DataTableModule} from "angular-6-datatable";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    DataTableModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalendarModule,
+    FullCalendarModule,
+    DragDropModule,
+    ProgressBarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
